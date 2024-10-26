2eca15d5a329db514d770af5eef4be884f478116
Martin Lacunza <Martin.lacunza@estudiante.imagecampus.edu.ar>
2024-10-25 23:33:25 -0300

    Ahora el arma tiene munición y puede recargar

--- THDV - Martín Lacunza 1/Assets/Shooting.cs
+++ THDV - Martín Lacunza 1/Assets/Shooting.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Diagnostics;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
 
 public class Gun : MonoBehaviour{
 
@@ -9,15 +12,46 @@ public class Gun : MonoBehaviour{
     public float range = 100f;
     public float fireRate = 15f;
     public float impactForce = 30f;
+    public TextMeshProUGUI ammoDisplay;
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
 
     public Camera fpscamera;
 
+    public Animator animator;
+
     private float nextTimeToFire = 0f;
 
+    void Start ()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable ()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading", false);
+    }
     // Update is called once per frame
     void Update ()
     {
-        
+        if (isReloading)
+        {
+            return;
+        }
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && currentAmmo < maxAmmo)
+        {
+            StartCoroutine(Reload());
+        }
+
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire) 
         {
             nextTimeToFire = Time.time + 1f / fireRate;
@@ -25,9 +59,27 @@ public class Gun : MonoBehaviour{
         }
     }
 
+    IEnumerator Reload ()
+    {
+        isReloading = true;
+
+        animator.SetBool("Reloading", true);
+
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("Reloading", false);
+        yield return new WaitForSeconds(1f);
+
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+    }
+
+
     void Shoot ()
     {
 
+        currentAmmo--;
+        ammoDisplay.text = maxAmmo.ToString();
         RaycastHit hit;
         if (Physics.Raycast(fpscamera.transform.position, fpscamera.transform.forward, out hit, range))
         {
@@ -38,9 +90,6 @@ public class Gun : MonoBehaviour{
             {
                 enemy.TakeDamage(damage);
             }
-
-
         }
     }
 }
-
